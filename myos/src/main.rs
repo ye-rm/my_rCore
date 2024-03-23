78758fa394f1d16e368552b9b4f5a67b30e0880f
ye-rm <wenyuye@bupt.edu.cn>
2024-03-23 12:34:35 +0800

    my_first_commit

---
+++ myos/src/main.rs
@@ -0,0 +1,28 @@
+#![feature(panic_info_message)]
+#![no_main]
+#![no_std]
+#[macro_use]
+
+mod console;
+mod lang_items;
+mod sbi;
+
+use core::arch::global_asm;
+global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("hello world!"); 
+    panic!("shut down machine");
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0)}
+    });
+}
